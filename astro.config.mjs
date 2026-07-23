9b262e5418bbe2984322cd32758ac3eeb1dd070f
NaimBiswas <nayeembiswas2@gmail.com>
2026-07-23 14:39:56 +0600

    feat: add Google Analytics, update site URL to metadataconverter.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dataconverter.app',
+  site: 'https://metadataconverter.com',
   integrations: [sitemap()],
 });
